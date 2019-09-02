f0931742fb83a784f8f0b9c6b6e31c992f3f004a
Sydney Hsieh <sydneyhsieh@Sydneys-Mac-mini.local>
2019-09-02 11:56:15 -0700

    initial commit for my consoleio

---
+++ ioinput/CountdownSong/Example.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Example here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import chn.util.ConsoleIO;
+public class Example
+{
+   public static void main()
+    {   
+        ConsoleIO console = new ConsoleIO();
+        String bedJumpers;
+        
+        //Get User Input
+        System.out.println("Enter: ");
+        bedJumpers = console.readLine();
+       
+    }
+}
---
+++ ioinput/consoleio/Test.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        SimpleIO simpleio = new SimpleIO();        
+        simpleio.askQuestion();
+        
+    }
+
+    
+}
