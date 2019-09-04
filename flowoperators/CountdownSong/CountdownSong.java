a8223b933b19d7948b468617cd72819278c1e5a5
pdimagiba <42821744+pdimagiba@users.noreply.github.com>
2019-09-04 12:10:41 -0700

    September 4 update

--- flowoperators/CountdownSong/CountdownSong.java
+++ flowoperators/CountdownSong/CountdownSong.java
@@ -5,8 +5,9 @@
  * @author (John Mortensen)
  * @version (a version number or a date)
  */
+import java.util.Scanner; // import the Scanner class 
 
-import java.util.Scanner;   //Import Scanner
+
 
 public class CountdownSong
 { // everything in Java is a class, we will learn more latter
@@ -14,36 +15,38 @@ public class CountdownSong
     public static void main()
     { // code blocks are in curly braces {}
         // initialise control variables for program
-        int countdown;                 // integer variable assignment, statements end in semi colon
-        String bedJumpers;
-        Scanner input = new Scanner(System.in);
+        int countdown = 20;  
+       
+        System.out.println("Enter your desired bed jumper:");
+        Scanner myObj = new Scanner(System.in);
+        String bedJumpers = myObj.nextLine();
         
-        //Get User Input
-        System.out.println("Enter the name of the bedjumpers: ");
-        bedJumpers = input.nextLine();
-        System.out.println("Enter the number of jumps: ");
-        countdown = input.nextInt();
+        System.out.println("Enter your desired number of bed jumpers:");
+    
+        countdown = myObj.nextInt();
+    
+    
+        // integrer variable assignment, statements end in semi colon
+        //String bedJumpers = "monkeys";      // string of characters 
 
         // prepare for countdown loop
         int i = countdown;
         String space = " ";
-        while (i > -1) { // while is a control flow statement, observe blocks of code
+        while (i > 0) { // while is a control flow statement, observe blocks of code
 
-            if (i == 0) { // if and else are control flow statments
-                // last message
-                System.out.println("Zero little" + space + bedJumpers);
-            } else {
                 // messages print each time through loop,  println prints a line to console, plus sign (i + space...) concatonates
                 System.out.println(i + space + "little" + space + bedJumpers + space + "jumping on the bed");
                 System.out.println("One fell off and broke his head");
                 System.out.println("Mama called the doctor and the doctor said");
-                System.out.println("no more monkeys jumping on the bed.");
-            }
+                System.out.println("No more" + bedJumpers + "jumping on the bed.");
+            
             
             // decrement i at end of loop
             i--;  // this is equivalent to i = i - 1, -- is a shorthand
             
         }    
+        System.out.println("Zero little" + space + bedJumpers);
     }
+
     
 }
