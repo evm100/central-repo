bf4f624d224ccaa3b8a79bf221ac9d2c659feb6b
JaredJose <42820125+JaredJose@users.noreply.github.com>
2019-09-06 13:48:51 -0700

    Created the Objects ExamLogoFinal for Project Manager team

---
+++ logodesign/Quadratic/Main.java
@@ -0,0 +1,21 @@
+import apcslib.*;
+import chn.util.*;
+import java.util.*;
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Main
+{
+   public static void main(){
+       int a, b, c;
+       int root1, root2;
+       ConsoleIO input = new ConsoleIO();
+       
+       System.out.print("Please enter an 'a' value: ");
+       a = input.
+    }
+}
